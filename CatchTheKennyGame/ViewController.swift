61bd0c60d3f348c0f3412018f3d2093e259693d0
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-07-13 11:22:57 +0300

    Initial Commit

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Atil Samancioglu on 13.07.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
